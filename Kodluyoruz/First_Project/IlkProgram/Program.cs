cafe5cdacfac4fdb56404496a3068905cf559792
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-04 16:55:06 +0300

    .Net İlk Odev

---
+++ Kodluyoruz/First_Project/IlkProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IlkProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
