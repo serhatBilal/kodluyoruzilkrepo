edc337c2847da59323adfcf493178e7900cb5b04
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-04 18:05:20 +0300

    Operators

---
+++ Kodluyoruz/First_Project/Operators/operators/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 2;
+            sayi1 = sayi1+2;
+            Console.WriteLine(sayi1);
+            int sayi2 = 10;
+            sayi2++;
+            Console.WriteLine(sayi2);
+
+            int sayi3 = 30;
+            sayi3 /=10;
+            Console.WriteLine(sayi3);
+
+            int sayi4 = 21;
+            int islem = sayi4%2;
+            Console.WriteLine(islem);
+
+            Console.WriteLine(sayi1==sayi3);
+            Console.WriteLine(sayi1<=sayi3);
+            Console.WriteLine(sayi1>=sayi3);
+            Console.WriteLine(sayi1!=sayi3);
+
+            if(sayi1>=sayi3 && sayi1!=sayi3){
+                Console.WriteLine("Harika");
+            }
+
+             if(sayi1>=sayi3 || sayi1!=sayi3){
+                Console.WriteLine("Harika");
+            }
+
+             if(sayi1>=sayi3 && !(sayi1!=sayi3)){
+                Console.WriteLine("Harika");
+            }
+
+
+
+            
+        }
+    }
+}
