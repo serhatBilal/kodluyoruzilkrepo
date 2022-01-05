1049c7ff78fed75544046d38da97ee7c0bea33eb
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-05 22:19:42 +0300

    if else ternary

---
+++ Kodluyoruz/First_Project/if Else/ifElse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time < 18){
+                Console.WriteLine("Tünaydın");
+            }
+            else if(time < 12){
+                Console.WriteLine("Günadyin");
+            }
+            else{
+                Console.WriteLine("Iyi Geceler");
+            }
+
+            String sonuc = time < 21 ? "Good Afternoon" : "Good Night";
+            Console.WriteLine(sonuc);
+
+            String sonuc2 = time >= 6 && time < 12 ? "Good Morning" : time < 18 ? "Good Afternoon" : "Good night "; 
+            Console.WriteLine(sonuc2);
+        }
+    }
+}
