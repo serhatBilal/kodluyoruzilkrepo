09a57d31ebab6169bfa27362a6eafd1c7f57af11
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-05 21:55:22 +0300

    Try Catch Finally

---
+++ Kodluyoruz/First_Project/Try Catch Finally/tcf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tcf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+               int a = Convert.ToInt32("serhat");  
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Bir sorun olustu" + " " + e);
+                
+            }
+            finally{
+                Console.WriteLine("Islem Tamamlandı");
+            }
+        }
+    }
+}
