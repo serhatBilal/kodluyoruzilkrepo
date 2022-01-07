74107198ad57e582e3a666a0ca4d060e0bace6a0
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-07 15:05:56 +0300

    While Foreach

---
+++ Kodluyoruz/First_Project/While Foreach Loop/Loops2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+            while(sayac <= sayi){
+
+                toplam+=sayac;
+                sayac++;
+
+            }
+            Console.WriteLine(toplam/sayi);
+
+            char character = 'a';
+            while (character <= 'z'){
+                Console.Write(character);
+                character++;
+            }
+
+            string[] arabalar = {"Audi","BMW","VOLVO"};
+
+            foreach(var araba in arabalar){
+                Console.WriteLine(araba);
+            }
+    }
+}
+
+       
+}
