fea5285d7bb5aebdb6aea3a7e5fdfddae36b3c95
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-07 20:25:07 +0300

    Array metot

---
+++ Kodluyoruz/First_Project/Array metod/ArrayMetot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ArrayMetot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dizi = new string[3];
+            dizi[0]="Mavi";
+            dizi[1]="Kirmizi";
+            dizi[2]="Beyaz";
+
+            foreach (var x in dizi)
+            {
+                Console.WriteLine(x);
+            }
+            //Siralanmis dizi
+            Array.Sort(dizi);
+            foreach (var x in dizi)
+            {
+                Console.WriteLine(x);
+            }
+            // Eleman silme
+            Array.Clear(dizi,0,1); // 0. indexten sonra 1 adet eleman sil.
+             foreach (var x in dizi)
+            {
+                Console.WriteLine(x);
+            }
+
+            //Dizi sıralamasını terse cevir
+
+            Array.Reverse(dizi);
+             foreach (var x in dizi)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+        }
+    }
+}
