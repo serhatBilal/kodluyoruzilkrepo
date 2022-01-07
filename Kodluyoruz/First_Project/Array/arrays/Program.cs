7a0ebf709015f270d0843816808de59b72bd3610
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-07 15:31:05 +0300

    Arrays

---
+++ Kodluyoruz/First_Project/Array/arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[5];
+            sayilar[1]= 10;
+            Console.WriteLine(sayilar[1]);
+
+
+            string[] renkler = {"Mavi","Kırmızı","Beyaz"};
+            Console.WriteLine(renkler[2]);
+
+            foreach(var renk in renkler){
+                Console.WriteLine(renk);
+            }
+        }
+    }
+}
