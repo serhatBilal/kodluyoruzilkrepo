6843c12e80fe3da1f750e471dbf8941ff63d6c8e
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-08 22:05:25 +0300

    Recursive ve Extension Metotlar

---
+++ Kodluyoruz/First_Project/Metotlar 3/metotlar3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace metotlar3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Expo(2,4));
+            string text = "Serhat BILAL";
+
+           Console.WriteLine(text.CheckSpaces()); 
+           Console.WriteLine(text.MakeUpperCase()); 
+        }
+
+        public static int Expo(int sayi,int üs){
+            if(üs<2){
+                return sayi; 
+            }
+                
+            return Expo(sayi,üs-1)*sayi;
+        }
+    }
+
+   public static class Extension{
+        public static bool CheckSpaces(this string param){
+            return param.Contains(" ");
+        }
+
+        public static string MakeUpperCase(this string param){
+            return param.ToUpper();
+        }
+
+    }
+}
