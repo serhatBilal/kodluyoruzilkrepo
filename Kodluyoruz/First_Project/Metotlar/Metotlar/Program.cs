27dd55e22790787234d1e006cd5c65e6b63744e2
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-08 20:51:03 +0300

    Metotlar

---
+++ Kodluyoruz/First_Project/Metotlar/Metotlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Topla(5,2));
+
+            Metotlar metot = new Metotlar();
+            metot.EkranaYaz("Serhat BILAL");
+        }
+
+        static int Topla(int a,int b){
+            return a+b;
+        }
+    }
+
+    class Metotlar{
+        public void EkranaYaz(String value){
+
+            Console.WriteLine(value);
+            
+        }
+    }
+}
