47fb462bb3b6cf9d7ccb2712df0b2c3a74146677
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-09 19:18:02 +0300

    Hazır string metotlar

---
+++ Kodluyoruz/First_Project/Hazır String Metotlar/HazirMetotlarString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HazirMetotlarString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Merhaba Dünya Merhaba";
+            string text2 = "Selam Dünya Selam";
+
+            Console.WriteLine(text.Length);
+            Console.WriteLine(text.ToLower());
+            Console.WriteLine(text.ToUpper());
+            Console.WriteLine(String.Concat(text,"Nasilsiniz"));
+            Console.WriteLine(text2.Contains("Dünya"));
+            Console.WriteLine(text2.EndsWith("Selam"));
+            Console.WriteLine(text2.StartsWith("Dünya"));
+            Console.WriteLine(text.Split(' ')[0]);
+            
+        }
+    }
+}
