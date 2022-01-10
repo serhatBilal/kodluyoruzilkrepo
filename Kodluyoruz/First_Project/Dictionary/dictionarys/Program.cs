6b8f1a386775e6201e7e4fdf4537c68a2cc7b326
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-10 16:44:55 +0300

    Dictionary

---
+++ Kodluyoruz/First_Project/Dictionary/dictionarys/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionarys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,string> kullanici = new Dictionary<int, string>();
+
+            kullanici.Add(1,"Serhat BİLAL");
+            kullanici.Add(2,"Nurseven BERKER");
+            kullanici.Add(3,"Ahmet Ahmet");
+
+          
+
+            Console.WriteLine(kullanici.Count);
+            Console.WriteLine(kullanici.ContainsKey(1));
+            Console.WriteLine(kullanici.ContainsValue("Nurseven BERKER"));
+
+            kullanici.Remove(3);
+
+              foreach(var x in kullanici.Values){
+                Console.WriteLine(x);
+            }
+
+
+        }
+    }
+}
