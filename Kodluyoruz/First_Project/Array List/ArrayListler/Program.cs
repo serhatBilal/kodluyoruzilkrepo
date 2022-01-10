abc95556c8811091d9a8f4e774a1d6a31a5fa2d6
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-10 16:28:54 +0300

    Array List

---
+++ Kodluyoruz/First_Project/Array List/ArrayListler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = {11,12,13};
+            ArrayList liste = new ArrayList();
+            liste.Add(1);
+            liste.Add(4);
+            liste.Add(5);
+            liste.Add(2);
+            liste.Add(77);
+
+            
+            liste.AddRange(dizi);
+            liste.Sort();
+
+            Console.WriteLine("Elemanın index numarasi: "+liste.BinarySearch(77));
+
+            liste.Reverse();
+
+            liste.Clear();
+
+            foreach(var x in liste){
+                 Console.WriteLine(x);
+            }
+
+
+
+            
+            
+            
+
+        }
+    }
+}
