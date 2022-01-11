3fc45c32e58d467153add3eb55bd49f4226f04f9
Serhat <62702297+serhatBilal@users.noreply.github.com>
2022-01-11 18:41:06 +0300

    Koleksiyonlar Odev 3

--- Kodluyoruz/First_Project/Koleksiyonlar Ödev/Ödev 3/KoleksiyonOdev3/Program.cs
+++ Kodluyoruz/First_Project/Koleksiyonlar Ödev/Ödev 3/KoleksiyonOdev3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace KoleksiyonOdev3
 {
@@ -7,8 +8,25 @@ namespace KoleksiyonOdev3
         static void Main(string[] args)
         {
             /*
-            Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+            Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde
+            saklayan ve dizinin elemanlarını sıralayan programı yazınız.
              */
+            string sesli="aeıioöuüAEIİOÖUÜ";
+            ArrayList sesliler = new ArrayList();
+
+            Console.Write("Cümleyi giriniz : ");
+            string cumle = Console.ReadLine();
+            for(int i=0;i<cumle.Length;i++){
+                for(int j=0;j<sesli.Length;j++){
+                    if(cumle[i]==sesli[j]){
+                        sesliler.Add(cumle[i]);
+                    }
+                }
+            }
+
+            foreach(var x in sesliler){
+                Console.Write(x + ",");
+            }
         }
     }
 }
